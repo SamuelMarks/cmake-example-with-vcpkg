993ec593d7b0f26dbd2c7eac3953a894081e69bd
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-07-07 00:36:09 +1000

    [tutorial.c] Abstract Windows difference to macro
    Signed-off-by: Samuel Marks <807580+SamuelMarks@users.noreply.github.com>

--- tutorial.c
+++ tutorial.c
@@ -7,36 +7,28 @@
 #include "TutorialConfig.h"
 
 #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+
 /* https://docs.microsoft.com/en-us/cpp/c-runtime-library/path-field-limits */
 #define PATH_SEP "\\"
 #define MAX_FNAME _MAX_FNAME
+#define strtok(str,delim) strtok_s(str,delim,NULL)
+#define strcpy(dst,src) strcpy_s(dst,strlen(dst),src)
+
 #else
+
 #define PATH_SEP "/"
 #define MAX_FNAME PATH_MAX
+
 #endif
 
 int main(int argc, char *argv[]) {
     {
         char basename[MAX_FNAME] = {0};
         {
-/* TODO: #define macros for STRTOK and STRCPY */
-#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-            char *token = strtok_s(argv[0], PATH_SEP, NULL);
-#else
+
             char *token = strtok(argv[0], PATH_SEP);
-#endif
-            for (; token;
-#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-            token = strtok_s(NULL, PATH_SEP, NULL)
-#else
-                token = strtok(NULL, PATH_SEP)
-#endif
-                    )
-#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-                strcpy_s(basename, strlen(token), token);
-#else
-            strcpy(basename, token);
-#endif
+            for (; token; token = strtok(NULL, PATH_SEP))
+                strcpy(basename, token);
         }
 
         printf("%s version: %4d.%d\n"
