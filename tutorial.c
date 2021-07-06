7a085739e0d5b8b413fddabf44d21bfc8f221766
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-07-07 00:14:56 +1000

    [tutorial.c] Windows support
    Signed-off-by: Samuel Marks <807580+SamuelMarks@users.noreply.github.com>

--- tutorial.c
+++ tutorial.c
@@ -8,6 +8,7 @@
 
 #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
 /* https://docs.microsoft.com/en-us/cpp/c-runtime-library/path-field-limits */
+#define PATH_SEP "\\"
 #define MAX_FNAME _MAX_FNAME
 #else
 #define PATH_SEP "/"
@@ -18,9 +19,24 @@ int main(int argc, char *argv[]) {
     {
         char basename[MAX_FNAME] = {0};
         {
+/* TODO: #define macros for STRTOK and STRCPY */
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+            char *token = strtok_s(argv[0], PATH_SEP, NULL);
+#else
             char *token = strtok(argv[0], PATH_SEP);
-            for (; token; token = strtok(NULL, PATH_SEP))
-                strcpy(basename, token);
+#endif
+            for (; token;
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+            token = strtok_s(NULL, PATH_SEP, NULL)
+#else
+                token = strtok(NULL, PATH_SEP)
+#endif
+                    )
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+                strcpy_s(basename, strlen(token), token);
+#else
+            strcpy(basename, token);
+#endif
         }
 
         printf("%s version: %4d.%d\n"
